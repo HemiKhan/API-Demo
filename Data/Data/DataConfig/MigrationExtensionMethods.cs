4d31a21fcac773527914c624e136ced141f65153
HammasKhan007 <114137475+HemiKhan@users.noreply.github.com>
2023-03-10 00:41:18 +0500

    .

---
+++ Data/Data/DataConfig/MigrationExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+namespace Data.DataConfig;
+
+internal static class MigrationExtensionMethods
+{
+    internal static void AddSqlFile(this MigrationBuilder migrationBuilder)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var sqlFiles = assembly.GetManifestResourceNames().
+                    Where(file => file.EndsWith(".sql"));
+        foreach (var sqlFile in sqlFiles)
+        {
+            using var stream = assembly.GetManifestResourceStream(sqlFile);
+            using var reader = new StreamReader(stream!);
+            var sqlScript = reader.ReadToEnd();
+            migrationBuilder.Sql($"EXEC(N'{sqlScript.Replace("'", "''")}')");
+        }
+    }
+}
---
+++ Data/Data/Migrations/20230309192608_SP_Add.cs
@@ -0,0 +1,20 @@
+﻿using Data.DataConfig;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class SP_Add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddSqlFile();
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
